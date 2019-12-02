330771ecdfee325fdb2b91e9fb5cf7d7ef1cfee0
Kawfang3025 <5930213025@email.psu.ac.th>
2019-12-02 13:58:09 +0700

    testcase correctly

---
+++ platforms/android/app/build/intermediates/merged_assets/debug/mergeDebugAssets/out/www/js/sidebar.js
@@ -0,0 +1,69 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+var email = localStorage.getItem("Email");
+console.log(email);
+
+var db = firebase.firestore();
+db.collection("employees").where("email", "==", email)
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+            document.getElementById("fname").innerHTML = doc.data().firstName + " " + doc.data().lastName;
+            document.getElementById("position").innerHTML = doc.data().positions;
+            document.getElementById("navpic").src = doc.data().img;
+            localStorage.setItem("position", doc.data().positions);
+            localStorage.setItem("department", doc.data().department);
+            localStorage.setItem("myID", doc.data().employeeID);
+            localStorage.setItem("mydoc", doc.id);
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+db.collection("technicain").where("email", "==", email)
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+            document.getElementById("fname").innerHTML = doc.data().firstName + " " + doc.data().lastName;
+            document.getElementById("position").innerHTML = doc.data().positions;
+            document.getElementById("navpic").src = doc.data().img;
+            localStorage.setItem("position", doc.data().positions);
+            localStorage.setItem("department", doc.data().department);
+            localStorage.setItem("myID", doc.data().technicainID);
+            localStorage.setItem("mydoc", doc.id);
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+
+// function logout() {
+   
+//     // Initialize Firebase
+//     firebase.initializeApp(firebaseConfig);
+//     firebase.auth().signOut().then(function() {
+//         // Sign-out successful.
+        
+//         alert('See you again soon').then(function() {
+//             localStorage.clear("Email");
+
+//         });
+//         window.location.href = 'index.html';
+//     }).catch(function(error) {
+//         // An error happened.
+//     });
+// }
